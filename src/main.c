48a415534974b0322567f2490700cbaa44025c1b
Nicolas Beny <nicolasbeny@Nicolass-iMac.local>
2017-03-04 11:39:16 +0100

    maj

--- src/main.c
+++ src/main.c
@@ -1,2 +1,26 @@
-#include "ft_ls.h"
+#include "../include/ft_ls.h"
 
+int		ft_ls(char *str)
+{
+	struct dirent	*d;
+	DIR		*dir;
+
+	if ((dir = opendir(str)) == NULL)
+		return (0);
+	while ((d = readdir(dir)))
+		printf("%s\n", d->d_name);
+	return (0);
+}
+
+int		main(int ac, char **av)
+{
+	int i;
+
+	i = 0;
+	(void)ac;
+	if (av[1] == NULL)
+		ft_ls(".");
+	while (av[++i] != NULL)
+		ft_ls(av[i]);
+	return (0);
+}
